1dbef8944794b598932417beb2efdce1378648a4
mxsf23 <154364804+mxsf23@users.noreply.github.com>
2024-04-17 22:17:22 +0300

    Update get_swarm_key.sh

--- terraform/scripts/get_swarm_key.sh
+++ terraform/scripts/get_swarm_key.sh
@@ -3,6 +3,6 @@
 ssh_remoteaddr=$1
 ssh_port=$2
 
-token=$(ssh -i /opt/git_projects/b48/sftest -o StrictHostKeyChecking=no -p $2 ansible@"$1" \
+token=$(ssh -i files/sftest -o StrictHostKeyChecking=no -p $2 ansible@"$1" \
    "sudo docker swarm join-token worker" | grep '[--]token' | awk '{print $5}')
 echo '{"token": "'"$token"'"}'
